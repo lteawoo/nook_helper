de0f974a8505f3cfda3e973f3161e60de1f438db
Taewoo Lee <lteawoo@naver.com>
2020-05-05 14:53:28 +0900

    topbar 추가

--- lib/main.dart
+++ lib/main.dart
@@ -12,8 +12,54 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Nooks Helper',
       home: Scaffold(
-        body: Catalog(),
+        body: Column(
+          children: <Widget>[
+            _TopBar(),
+            Expanded(
+              child: Catalog(),
+            ),
+          ],
+        ),
       ),
+    );
+  }
+}
+
+class _TopBar extends StatelessWidget {
+  const _TopBar({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final spacing = const SizedBox(width: 30);
+    return Container(
+        width: double.infinity,
+        margin: const EdgeInsets.only(top: 8),
+        padding: EdgeInsets.symmetric(horizontal: 30),
+        child: Wrap(
+            alignment: WrapAlignment.spaceBetween,
+            children: [
+              Row(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  ExcludeSemantics(
+                      child: SizedBox(
+                          height: 80
+                      )
+                  ),
+                  spacing,
+                  Text(
+                      '너굴 도우미',
+                      style: Theme.of(context).textTheme.bodyText1.copyWith(
+                        fontSize: 35,
+                        fontWeight: FontWeight.w600,
+                      )
+                  ),
+                ],
+              ),
+            ]
+        )
     );
   }
 }
